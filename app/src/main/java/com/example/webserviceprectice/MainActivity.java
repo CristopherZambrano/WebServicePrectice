8f4512b76f6cfa25ca2a1f45e10d8ceeba2b6301
CristopherZambrano <79436828+CristopherZambrano@users.noreply.github.com>
2021-12-07 23:22:46 -0500

    Envio de informacion

--- app/src/main/java/com/example/webserviceprectice/MainActivity.java
+++ app/src/main/java/com/example/webserviceprectice/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.webserviceprectice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void btnEnviar(View view) {
+        //Creamos el Intent
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        EditText txtNombre = (EditText) findViewById(R.id.txtxname);
+        EditText txtpass = (EditText) findViewById(R.id.txtPassword);
+        //Creamos la información a pasar entre actividades - Pares Key-Value
+        Bundle b = new Bundle();
+        b.putString("name", txtNombre.getText().toString());
+        b.putString("pass", txtpass.getText().toString());
+        //Añadimos la información al intent
+        intent.putExtras(b);
+        // Iniciamos la nueva actividad
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/webserviceprectice/MainActivity2.java
+++ app/src/main/java/com/example/webserviceprectice/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.webserviceprectice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,6 +11,13 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        //Localizar los controles
+        TextView txtSaludo = (TextView) findViewById(R.id.txtLista);
+        //Recuperamos la información pasada en el intent
+        Bundle bundle = this.getIntent().getExtras();
+        //Construimos el mensaje a mostra
+        txtSaludo.setText("Hola!, Bienvenido \n " + bundle.getString("name")+"password: "+ bundle.getString("pass"));
+
     }
 }
-//Probando los commits
