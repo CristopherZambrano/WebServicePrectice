6c8659b3a21556e3a54ffb89b77056763bb3665f
CristopherZambrano <79436828+CristopherZambrano@users.noreply.github.com>
2021-12-07 23:52:41 -0500

    Api implementacion semicompleta

--- app/src/main/java/com/example/webserviceprectice/MainActivity2.java
+++ app/src/main/java/com/example/webserviceprectice/MainActivity2.java
@@ -5,7 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity2 extends AppCompatActivity {
+import com.example.webserviceprectice.webservice.Asynchtask;
+import com.example.webserviceprectice.webservice.WebService;
+
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity2 extends AppCompatActivity implements Asynchtask {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,11 +21,22 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         //Localizar los controles
-        TextView txtSaludo = (TextView) findViewById(R.id.txtLista);
-        //Recuperamos la información pasada en el intent
-        Bundle bundle = this.getIntent().getExtras();
+        //TextView txtSaludo = (TextView) findViewById(R.id.txtLista);
+        //Recuperamos la información pasada en el inten
         //Construimos el mensaje a mostra
-        txtSaludo.setText("Hola!, Bienvenido \n " + bundle.getString("name")+"password: "+ bundle.getString("pass"));
+        //txtSaludo.setText("Hola!, Bienvenido \n " + bundle.getString("name")+"password: "+ bundle.getString("pass"));
 
+        Bundle bundle = this.getIntent().getExtras();
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws= new WebService("http://uealecpeterson.net/ws/login.php?usr="
+                       + bundle.getString("name") + "&pass=" + bundle.getString("pass"),
+                        datos, MainActivity2.this, MainActivity2.this);
+        ws.execute("GET");
+
+    }
+    @Override
+    public void processFinish(String result) throws JSONException{
+        TextView txtSaludo = (TextView) findViewById(R.id.txtLista);
+        txtSaludo.setText("RESPUESTA " + result);
     }
 }
